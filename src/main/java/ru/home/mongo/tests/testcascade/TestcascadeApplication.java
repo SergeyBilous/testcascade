a693561f1ebe69ea372364f5cf6eef07702bbce7
Sergey Bilous <bill-gh@yandex.ru>
2025-03-01 17:50:50 +0300

    Spring boot project template added

---
+++ src/main/java/ru/home/mongo/tests/testcascade/TestcascadeApplication.java
@@ -0,0 +1,13 @@
+package ru.home.mongo.tests.testcascade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcascadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcascadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/home/mongo/tests/testcascade/TestcascadeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.home.mongo.tests.testcascade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcascadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
